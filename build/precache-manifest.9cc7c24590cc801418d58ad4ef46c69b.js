1cb2d9284cd2592516c9479be55c6e44645eeb2f
sizplay@gmail.com <sizplay@gmail.com>
2018-12-09 15:24:10 -0500

    commit

---
+++ build/precache-manifest.9cc7c24590cc801418d58ad4ef46c69b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "769618a118da251dab76",
+    "url": "/static/css/main.4d5e2a17.chunk.css"
+  },
+  {
+    "revision": "769618a118da251dab76",
+    "url": "/static/js/main.769618a1.chunk.js"
+  },
+  {
+    "revision": "cdb6b5480881ea4af263",
+    "url": "/static/js/1.cdb6b548.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "089f2cc25000687b2a660a170b149c5f",
+    "url": "/static/media/Member_1.089f2cc2.jpeg"
+  },
+  {
+    "revision": "9bf7ec44c79d946c9e8a0614be235d82",
+    "url": "/static/media/Member_2.9bf7ec44.jpg"
+  },
+  {
+    "revision": "385efaa7c4c8a363b60f11356328415b",
+    "url": "/static/media/Member_3.385efaa7.jpg"
+  },
+  {
+    "revision": "bf39f528ea0194665879abde61f95e5e",
+    "url": "/index.html"
+  }
+];
